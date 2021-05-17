7886b46f9242b1f2876ad8af18986514f7c915d8
Rodrigo Tenorio <rodrigo.s.tenorio@gmail.com>
2021-05-17 19:00:14 +0200

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,5 +1,20 @@
 package com.devsuperior.dscatalog.services;
 
-public interface CategoryService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		
+		return repository.findAll();
+	}
 }
